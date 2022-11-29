5d874fff46765b772c6fed23b41331299eb1f94f
br80b <118027419+br80b@users.noreply.github.com>
2022-11-28 17:48:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 // I'm lost                         
 <console.log('fuck this is hard');    
 </script>  
+
+let quantity = '0';
+console.log(name);
+
+const quantity
+const price = 7.99;
+let total = price*quantity
